b5f469811e0c6d25e278e2fe5d9bc0a436eb5e9a
TayyabHahsmi <tayyabhashmi65@gmail.com>
2024-01-31 16:59:21 +0530

    updated model

--- models/example/dbt_channel_info.sql
+++ models/example/dbt_channel_info.sql
@@ -6,14 +6,14 @@ WITH total_view_count AS (
     SELECT
         SUM(viewcount) AS total_views
     FROM
-        sprinkle_channel_info
+       `Tayyab_dataset.dbt_sprinkle_channel_info'
 )
 
 SELECT
     sci.*,
     tvc.total_views
 FROM
-    sprinkle_channel_info sci
+   `Tayyab_dataset.dbt_sprinkle_channel_info` sci
 JOIN
     total_view_count tvc
 
