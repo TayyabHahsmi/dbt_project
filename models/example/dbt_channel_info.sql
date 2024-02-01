77ed12c1100e31b1e70ba6060b2f87f2cc6ff201
TayyabHahsmi <tayyabhashmi65@gmail.com>
2024-02-01 13:34:23 +0530

    finally

--- models/example/dbt_channel_info.sql
+++ models/example/dbt_channel_info.sql
@@ -4,7 +4,7 @@
 
 WITH total_view_count AS (
     SELECT
-        channelid,SUM(viewcount)AS total_views, subscriber, publishedat, updated_at 
+        SUM(viewcount)AS total_view_count 
     FROM
      `Tayyab_dataset.dbt_local_sprinkle_channel_info`
 )
