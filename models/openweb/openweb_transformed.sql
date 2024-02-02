b49738f087fe42d04588ad9c244bf89aa32a9034
TayyabHahsmi <tayyabhashmi65@gmail.com>
2024-02-02 15:12:04 +0530

    openweb

--- models/openweb/openweb_transformed.sql
+++ models/openweb/openweb_transformed.sql
@@ -1,11 +1,11 @@
 {{config(materialized='table')}}
 
 
-WITH transformed_openweb as ( SELECT *,
+WITH openweb as ( SELECT *,
   replace(substring(page_url from '.*://([^/]*)'), 'www.', '') AS url_domain
 FROM
   new_openweb)
 
-select * from trasnformed_openweb
+select * from openweb
 
 
